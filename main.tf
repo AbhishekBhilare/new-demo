fca78ea2bbc9c16e743997e9cfa92a2e8f79770c
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-01-12 12:05:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["t2.micro" , "m5.large", "m5n.large", "m5zn.large"]
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
 
   eks_managed_node_groups = {

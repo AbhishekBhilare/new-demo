df90d7eb33b4b5c776f16251baf7b6180c9d83ab
Abhishek Ankush Bhilare <69209913+AbhishekBhilare@users.noreply.github.com>
2023-01-12 13:37:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,32 +94,32 @@ module "eks" {
 
   aws_auth_roles = [
     {
-      rolearn  = "arn:aws:iam::66666666666:role/role1"
-      username = "role1"
+      rolearn  = "arn:aws:iam::272814864966:role/Terraform-role"
+      username = "Terraform-role"
       groups   = ["system:masters"]
     },
   ]
 
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user1"
-      username = "user1"
-      groups   = ["system:masters"]
-    },
-    {
-      userarn  = "arn:aws:iam::66666666666:user/user2"
-      username = "user2"
-      groups   = ["system:masters"]
-    },
-  ]
+#   aws_auth_users = [
+#     {
+#       userarn  = "arn:aws:iam::66666666666:user/user1"
+#       username = "user1"
+#       groups   = ["system:masters"]
+#     },
+#     {
+#       userarn  = "arn:aws:iam::66666666666:user/user2"
+#       username = "user2"
+#       groups   = ["system:masters"]
+#     },
+#   ]
 
-  aws_auth_accounts = [
-    "777777777777",
-    "888888888888",
-  ]
+#   aws_auth_accounts = [
+#     "777777777777",
+#     "888888888888",
+#   ]
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
+#   tags = {
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+# }
